1dba188fa5d3849a9460678c0fd304840b2d7338
yeojini <yesjin1209@gmail.com>
2024-09-21 18:40:54 +0900

    feat: 저장 버튼 구현

---
+++ components/summary/SummarySaveButton.tsx
@@ -0,0 +1,12 @@
+import Button from '@/components/ui/Button';
+import { FunctionComponent } from 'react';
+
+const SummarySaveButton: FunctionComponent = () => {
+  return (
+    <Button type="button" variant="rounded" className="h-23 w-24">
+      저장
+    </Button>
+  );
+};
+
+export default SummarySaveButton;
