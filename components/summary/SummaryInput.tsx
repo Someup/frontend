4577d43b2b6bf8fd20d83358f9d13e9e229060db
yeojini <yesjin1209@gmail.com>
2024-09-21 21:02:44 +0900

    refactor: input -> textarea 로 변경

--- components/summary/SummaryInput.tsx
+++ components/summary/SummaryInput.tsx
@@ -4,6 +4,10 @@ import { useState, ChangeEvent, KeyboardEvent } from 'react';
 import { useRouter } from 'next/navigation';
 import { useMutation } from '@tanstack/react-query';
 import { createSummary } from '@/lib/service/summary/summaryService';
+import TextField from '@/components/ui/TextField';
+import Button from '@/components/ui/Button';
+import { typography } from '@/styles/typography';
+import { cn } from '@/lib/utils';
 
 const SummaryInput = () => {
   const [url, setUrl] = useState('');
@@ -28,27 +32,42 @@ const SummaryInput = () => {
     });
   };
 
-  const handleKeyDown = async (e: KeyboardEvent<HTMLInputElement>) => {
+  const handleKeyDown = async (e: KeyboardEvent<HTMLTextAreaElement>) => {
     if (e.key === 'Enter') {
       handleSummary();
     }
   };
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     setUrl(e.target.value);
   };
 
+  const currentOptions = '상세 요약, 비공식적 말투, 기본값(한국어)';
+
   return (
     <>
-      <input
-        type="url"
+      <TextField
         placeholder="URL을 입력해주세요."
         onChange={handleChange}
         onKeyDown={handleKeyDown}
-      />
-      <button onClick={handleSummary} disabled={isPending}>
-        {isPending ? '요약 중...' : '요약하기'}
-      </button>
+        className="max-w-[73.89%]"
+      >
+        <div className="flex items-end justify-between">
+          <span
+            className={cn(typography({ scale: 'body-4' }), 'text-gray-800')}
+          >
+            {currentOptions}
+          </span>
+          <Button
+            variant="filled"
+            onClick={handleSummary}
+            disabled={isPending}
+            className="rounded-11"
+          >
+            요약하기
+          </Button>
+        </div>
+      </TextField>
     </>
   );
 };
