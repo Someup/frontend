09f193fa33f216de999572fdbc203d85443fc909
qaws7791 <qaws7791@gmail.com>
2024-09-23 20:21:18 +0900

    refactor: chip 컴포넌의 close icon 분리

--- components/ui/Chip.tsx
+++ components/ui/Chip.tsx
@@ -1,3 +1,4 @@
+import ClosOutlined from '@/components/icons/CloseOutlined';
 import { cn } from '@/lib/utils';
 import React from 'react';
 
@@ -25,16 +26,7 @@ const Chip = ({
           onClick={onClose}
           className="ml-1 rounded-full p-1 hover:bg-gray-50 focus:bg-gray-50 focus:outline-none focus:ring-1 focus:ring-tertiary"
         >
-          <svg
-            className="h-4 w-4"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z" />
-          </svg>
+          <ClosOutlined className="h-4 w-4" />
         </button>
       )}
     </div>
