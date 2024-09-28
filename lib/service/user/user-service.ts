1606da30018e7efe423cee11ec2340aa9d519769
qaws7791 <qaws7791@gmail.com>
2024-09-29 00:23:43 +0900

    refactor: fetchProfile 함수에서 data 타입을 as 대신 제네릭으로 사용

--- lib/service/user/user-service.ts
+++ lib/service/user/user-service.ts
@@ -2,5 +2,5 @@ import httpClient from '@/lib/http-client';
 import { GetUserResponse } from '@/types/user-types';
 
 export async function fetchProfile() {
-  return httpClient.get('/users').then((res) => res.data as GetUserResponse);
+  return httpClient.get<GetUserResponse>('/users').then((res) => res.data);
 }
