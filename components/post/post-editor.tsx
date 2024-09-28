bc415340c7580f140b2ce18ee57660d9eb399368
yeojini <yesjin1209@gmail.com>
2024-09-28 21:51:27 +0900

    feat: 태그 추가/삭제 기능 구현

--- components/post/post-editor.tsx
+++ components/post/post-editor.tsx
@@ -1,5 +1,6 @@
 'use client';
 import Editor from '@/components/editor/editor';
+import Chip from '@/components/ui/Chip';
 import Input from '@/components/ui/Input';
 import { usePostDetail } from '@/lib/service/post/use-post-service';
 import { FunctionComponent, useState, ChangeEvent } from 'react';
@@ -9,19 +10,61 @@ interface PostEditorProps {
 }
 const PostEditor: FunctionComponent<PostEditorProps> = ({ id }) => {
   const {
-    data: { content, title },
+    data: { content, title, tagList },
   } = usePostDetail({ id });
 
   const [newTitle, setNewTitle] = useState(title);
+  const [newTagList, setNewTagList] = useState(tagList);
+  const [newTag, setNewTag] = useState('');
 
   const handleTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setNewTitle(e.target.value);
   };
+
+  const deleteTag = (tag: string) => {
+    setNewTagList(newTagList.filter((t) => t !== tag));
+  };
+
+  const handleTagChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setNewTag(e.target.value);
+  };
+
+  const addNewTag = () => {
+    if (!newTag || newTagList.length >= 5) {
+      return;
+    }
+    if (newTagList.includes(newTag)) {
+      setNewTag('');
+      return;
+    }
+    setNewTagList([...newTagList, newTag]);
+    setNewTag('');
+  };
+
+  const isInsertTagEnable = newTagList.length < 5;
+
   return (
     <>
       <Editor markdown={content} readOnly />
       <div>
         <Input value={newTitle} onChange={handleTitleChange} />
+        <div>
+          {newTagList.map((tag) => (
+            <Chip key={tag} onClose={() => deleteTag(tag)}>
+              {tag}
+            </Chip>
+          ))}
+          {isInsertTagEnable && (
+            <input
+              type="text"
+              placeholder="태그를 입력하세요"
+              value={newTag}
+              onKeyDown={(e) => e.key === 'Enter' && addNewTag()}
+              onChange={handleTagChange}
+              onBlur={addNewTag}
+            />
+          )}
+        </div>
         <Editor markdown={content} />
       </div>
     </>
