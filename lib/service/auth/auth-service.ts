f9317de7899d3f3a9b9d74fd6424fef2125b9055
qaws7791 <qaws7791@gmail.com>
2024-09-30 14:52:50 +0900

    feat: loginKakao 함수로 분리

--- lib/service/auth/auth-service.ts
+++ lib/service/auth/auth-service.ts
@@ -1,7 +1,17 @@
 import clientEnv from '@/lib/env/client-env';
 import token from '@/lib/service/auth/token';
-import { ReissueTokenResponse } from '@/types/auth-types';
+import { LoginResponse, ReissueTokenResponse } from '@/types/auth-types';
 import axios from 'axios';
+
+export async function loginKakao(code: string) {
+  return axios.post<LoginResponse>(
+    `${clientEnv.NEXT_PUBLIC_API_BASE_URL}/auth/login/kakao?code=${code}`,
+    undefined,
+    {
+      withCredentials: true,
+    },
+  );
+}
 
 export function logout() {
   const accessToken = token.accessToken.get();
