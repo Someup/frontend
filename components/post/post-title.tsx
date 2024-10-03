70acf7075fc8645d69365676c72b6e5cd4c06973
yeojini <yesjin1209@gmail.com>
2024-10-03 19:04:49 +0900

    feat: post title 유효성 검사 추가

--- components/post/post-title.tsx
+++ components/post/post-title.tsx
@@ -1,21 +1,20 @@
-import {
-  forwardRef,
-  useImperativeHandle,
-  useState,
-  ChangeEvent,
-  Ref,
-} from 'react';
+import { forwardRef, useImperativeHandle, useState, ChangeEvent } from 'react';
 
 interface TitleInputProps {
   initialTitle: string;
 }
 
 const PostTitle = forwardRef<{ getTitle: () => string }, TitleInputProps>(
-  ({ initialTitle }, ref: Ref<{ getTitle: () => string }>) => {
+  ({ initialTitle }, ref) => {
     const [title, setTitle] = useState(initialTitle);
+    const [showError, setShowError] = useState(false);
 
     const handleTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
       setTitle(e.target.value);
+    };
+
+    const handleBlur = () => {
+      setShowError(title.length < 2 || title.length > 30);
     };
 
     useImperativeHandle(ref, () => ({
@@ -23,12 +22,17 @@ const PostTitle = forwardRef<{ getTitle: () => string }, TitleInputProps>(
     }));
 
     return (
-      <input
-        type="text"
-        value={title}
-        onChange={handleTitleChange}
-        className="flex-1 text-3xl font-semibold"
-      />
+      <div className="flex flex-col">
+        <input
+          value={title}
+          onChange={handleTitleChange}
+          className="flex-1 text-3xl font-semibold"
+          onBlur={handleBlur}
+        />
+        {showError && (
+          <p className="text-error">제목은 2자 이상 30자 이하여야 합니다.</p>
+        )}
+      </div>
     );
   },
 );
