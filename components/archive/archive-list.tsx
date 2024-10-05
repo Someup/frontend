22f60e3e6cba9780bb21e1867a4ac4394734d69f
qaws7791 <qaws7791@gmail.com>
2024-10-05 22:21:11 +0900

    feat: 아카이브 리스트 최하단에 아카이브 생성 버튼을 추가

--- components/archive/archive-list.tsx
+++ components/archive/archive-list.tsx
@@ -1,9 +1,20 @@
 'use client';
 import ArchiveListItem from '@/components/archive/archive-list-item';
-import { useArchives } from '@/lib/service/archive/use-archive-service';
+import CreateArchiveDialog from '@/components/archive/create-archive-dialog';
+import MaxArchiveNumAlert from '@/components/archive/max-archive-num-alert';
+import PlusOutlined from '@/components/icons/PlusOutlined';
+import { DialogTrigger } from '@/components/ui/Dialog';
+import { ARCHIVE_MAX_NUM } from '@/lib/service/archive/constraints';
+import {
+  useArchives,
+  useCreateArchive,
+} from '@/lib/service/archive/use-archive-service';
+import { useAllPostCount } from '@/lib/service/post/use-post-service';
 import { cn } from '@/lib/utils';
 import { typography } from '@/styles/typography';
+import { Dialog } from '@radix-ui/react-dialog';
 import Link from 'next/link';
+import { useState } from 'react';
 
 interface ArchiveListProps {
   selectedArchiveId?: string;
@@ -11,16 +22,42 @@ interface ArchiveListProps {
 
 export default function ArchiveList({ selectedArchiveId }: ArchiveListProps) {
   const archivesQuery = useArchives();
+  const postCountQuery = useAllPostCount();
+  const createArchive = useCreateArchive();
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [MaxArchiveAlertOpen, setMaxArchiveAlertOpen] = useState(false);
+
+  const createArchiveDisabled =
+    archivesQuery.data.archives.length > ARCHIVE_MAX_NUM;
+
+  const handleCreateArchive = (archiveName: string) => {
+    if (archivesQuery.data.archives.length > ARCHIVE_MAX_NUM) {
+      setIsDialogOpen(false);
+      setMaxArchiveAlertOpen(true);
+      return;
+    }
+    createArchive.mutate(
+      { name: archiveName },
+      {
+        onSuccess: () => {
+          setIsDialogOpen(false);
+        },
+      },
+    );
+  };
+
   return (
     <div className="flex flex-col">
       <Link
         href={`/archive`}
         className={cn(
           typography({ scale: 'body-5' }),
-          'rounded-2 px-5 py-3 text-gray-400 hover:bg-gray-50',
+          'flex items-center justify-between px-5 py-3',
+          !selectedArchiveId ? 'text-primary-400' : 'text-black',
         )}
       >
         전체
+        <span>({postCountQuery.data.totalCount})</span>
       </Link>
       {archivesQuery.data.archives.map((archive) => {
         return (
@@ -32,6 +69,24 @@ export default function ArchiveList({ selectedArchiveId }: ArchiveListProps) {
           />
         );
       })}
+      <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+        <DialogTrigger asChild>
+          <button
+            className={cn(
+              typography({ scale: 'body-5' }),
+              'flex items-center justify-between px-5 py-3 text-gray-400',
+            )}
+            disabled={createArchiveDisabled}
+          >
+            아카이브 추가
+            <PlusOutlined className="h-4 w-4" />
+          </button>
+        </DialogTrigger>
+        <CreateArchiveDialog onSubmit={handleCreateArchive} />
+      </Dialog>
+      <Dialog open={MaxArchiveAlertOpen} onOpenChange={setMaxArchiveAlertOpen}>
+        <MaxArchiveNumAlert />
+      </Dialog>
     </div>
   );
 }
