b684613cb4b67c85b0d87182af14f31fe5c1cfe8
yeojini <yesjin1209@gmail.com>
2024-10-05 20:56:49 +0900

    feat: 태그 목록 보기 추가
    - 기존 post tags 컴포넌트에 editable props 추가하여 편집 불가능하도록 처리

--- components/post/post-detail.tsx
+++ components/post/post-detail.tsx
@@ -7,6 +7,7 @@ import Editor from '@/components/editor/editor';
 import { PostStatus } from '@/types/post-types';
 import PostTitle from '@/components/post/post-title';
 import MemoTextField from '@/components/post/memo-text-field';
+import PostTags from '@/components/post/post-tags';
 
 interface PostDetailProps {
   id: string;
@@ -19,8 +20,10 @@ const PostDetail: FunctionComponent<PostDetailProps> = ({
   readOnly,
 }) => {
   const {
-    data: { content, url, title, memoContent, memoCreatedAt },
+    data: { content, url, title, memoContent, memoCreatedAt, tagList },
   } = usePostDetail({ id, status });
+
+  const isPublished = status === 'published';
 
   return (
     <div className="mx-auto flex-1">
@@ -37,7 +40,16 @@ const PostDetail: FunctionComponent<PostDetailProps> = ({
         {url}
       </a>
       <Editor markdown={content} readOnly={readOnly} />
-      <MemoTextField initialMemo={memoContent} createdAt={memoCreatedAt} />
+      {isPublished && (
+        <>
+          <PostTags
+            initialTagList={tagList}
+            editable={false}
+            className="mt-2"
+          />
+          <MemoTextField initialMemo={memoContent} createdAt={memoCreatedAt} />
+        </>
+      )}
     </div>
   );
 };
