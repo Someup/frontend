5ef28966b3c15f6715eaa41ef2085201ebea5ade
qaws7791 <qaws7791@gmail.com>
2024-10-09 04:03:38 +0900

    feat: preview 위치에 공통으로 toaster 추가

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,5 +1,7 @@
 import type { Preview } from '@storybook/react';
 import '../app/globals.css';
+import { Toaster } from '../components/ui/Toster';
+import React from 'react';
 
 const preview: Preview = {
   parameters: {
@@ -10,6 +12,14 @@ const preview: Preview = {
       },
     },
   },
+  decorators: [
+    (Story) => (
+      <div>
+        <Story />
+        <Toaster />
+      </div>
+    ),
+  ],
 };
 
 export default preview;
