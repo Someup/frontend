6f59872fbb44d941cfa2c24dc2772ff8c85086a3
yeojini <yesjin1209@gmail.com>
2024-10-16 21:46:23 +0900

    fix: 포스트 내용 유효성 검사 추가
    - react-hook-form의 Controller 컴포넌트로 content 필드를 제어하도록 수정
    - title, content 유효성 검사 시 toast 메시지와 focus 설정 추가
    - content의 길이를 실시간으로 감시하여 표시되도록 watch 사용
    - 에디터 내용 변경 시 textLength 업데이트 반영

--- lib/service/post/constraints.ts
+++ lib/service/post/constraints.ts
@@ -4,6 +4,11 @@ const POST_TITLE_MAX_LENGTH: number = 100;
 const POST_TITLE_MAX_LENGTH_ERROR: string = '제목을 100자 이내로 입력해주세요.';
 const POST_TITLE_MIN_LENGTH: number = 1;
 const POST_TITLE_MIN_LENGTH_ERROR: string = '제목을 입력하세요.';
+const POST_CONTENT_MIN_LENGTH: number = 1;
+const POST_CONTENT_MIN_LENGTH_ERROR: string = '내용을 입력하세요.';
+const POST_CONTENT_MAX_LENGTH_ERROR: string =
+  '5000자를 초과하여 입력할 수 없습니다.';
+export const POST_CONTENT_MAX_LENGTH: number = 5000;
 const POST_MEMO_MIN_LENGTH: number = 0;
 export const POST_MEMO_MAX_LENGTH: number = 1000;
 
@@ -12,6 +17,10 @@ export const postSchema = z.object({
     .string()
     .min(POST_TITLE_MIN_LENGTH, POST_TITLE_MIN_LENGTH_ERROR)
     .max(POST_TITLE_MAX_LENGTH, POST_TITLE_MAX_LENGTH_ERROR),
+  content: z
+    .string()
+    .min(POST_CONTENT_MIN_LENGTH, POST_CONTENT_MIN_LENGTH_ERROR)
+    .max(POST_CONTENT_MAX_LENGTH, POST_CONTENT_MAX_LENGTH_ERROR),
   memo: z.string().min(POST_MEMO_MIN_LENGTH).max(POST_MEMO_MAX_LENGTH),
 });
 
